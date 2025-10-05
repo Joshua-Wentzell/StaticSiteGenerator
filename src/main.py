9657decd80b8cb3a6a2b9fc1114729d59e45e5f9
jwentzel <wentzelljoshua047@gmail.com>
2025-10-05 12:47:15 -0400

    Project setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextType, TextNode
+
+
+def main():
+    textNode = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
+    print(repr(textNode))
+
+main()
