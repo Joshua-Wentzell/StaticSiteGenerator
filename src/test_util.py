acff0dbcf13ef096dbdf546c7d5b3a95abb85d7e
jwentzel <wentzelljoshua047@gmail.com>
2025-10-10 16:01:30 -0400

    Improve whitespace handling in markdown_to_blocks tests for consistency

--- src/test_util.py
+++ src/test_util.py
@@ -207,14 +207,14 @@ class TestUtil(unittest.TestCase):
 
     def test_markdown_to_blocks(self):
         md = """
-            This is **bolded** paragraph
+This is **bolded** paragraph
 
-            This is another paragraph with _italic_ text and `code` here
-            This is the same paragraph on a new line
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
-            - This is a list
-            - with items
-            """
+- This is a list
+- with items
+"""
         blocks = markdown_to_blocks(md)
         self.assertEqual(
             blocks,
@@ -263,13 +263,14 @@ class TestUtil(unittest.TestCase):
 
     def test_markdown_to_blocks_complex(self):
         md = """# Heading
-            Paragraph with multiple
-            lines of text
 
-            - List item 1
-            - List item 2
+Paragraph with multiple
+lines of text
 
-            Another paragraph"""
+- List item 1
+- List item 2
+
+Another paragraph"""
         blocks = markdown_to_blocks(md)
         self.assertEqual(blocks, [
             "# Heading",
